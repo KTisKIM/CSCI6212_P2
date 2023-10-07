3eb6babf21de77c206e85dbba1226a0b4c8be38b
Keuntae Kim <keuntaekim1994@gmail.com>
2023-10-06 22:24:44 -0400

    Must be a final commit

--- p2_program.py
+++ p2_program.py
@@ -16,49 +16,45 @@ Algorithm MergeSequence (Input: Array L[1..n], L[i] is the size of the i-th list
             // 2 log (n) time for two delete operations
         Add a new element corresponding to the merged list
             // log(n) time for one insert operation
+        Sum of the total cost and the size of the new merged list
 """
 
 # import heapq
-from heapq import heapify, heappop, heappush
 # import time
-from time import perf_counter_ns
 # import random
+from heapq import heapify, heappop, heappush
+from time import perf_counter_ns
 from random import choice
 
 # Finds the merge sequence, given an array
 # that contains the sizes of the lists
 def merge_sequence(nums_array):
-    start = perf_counter_ns()      ### Start time ###
+    start = perf_counter_ns()           ### Start time ###
 
     total_cost = 0
 
-    print("-----BEFORE heapify", nums_array)
     # Build the original heap
     heapify(nums_array)
-    print("-----AFTER heapify", nums_array)
     for i in range(1, len(nums_array)):
         # Remove two smallest elements from the heap - 2 log (n) time for two delete operations
         first_smallest = heappop(nums_array)
         second_smallest = heappop(nums_array)
-        print("-----FIRST:", first_smallest, "SECOND:", second_smallest)
-        print("-----AFTER popping", nums_array)
         # Add a new element corresponding to the merged list - log(n) time for one insert operation
         heappush(nums_array, first_smallest + second_smallest)
-        print('-----AFTER push', nums_array)
 
-        total_cost += first_smallest + second_smallest  # Total cost of merging all the lists
+        # Sum of the total cost and the size of the new merged list
+        total_cost += first_smallest + second_smallest
 
-    end = perf_counter_ns()        ### End time ###
-    print(f"Total Cost: {total_cost}")
-    print(f"{end - start} nanoseconds") ### Experimental result ###
+    end = perf_counter_ns()             ### End time ###
+    print(f"Running time: {end - start} nanoseconds") ### Experimental result ###
 
     return nums_array, total_cost
 
 
 if __name__ == "__main__":
-    n = 10  # need to define 'n'
-    zero_to_nine = list(range(10))
-    a = [choice(zero_to_nine) for i in range(n)]    # array of numbers
+    n = 10                                          # size of the array
+    zero_to_nine = list(range(10))                  # numbers between 0-9 for the input array.
+    a = [choice(zero_to_nine) for i in range(n)]    # array of numbers based on the size of 'n'
 
     final_nums_array, final_total_cost = merge_sequence(a)
-    print("\n최종 답", final_nums_array, final_total_cost)
+    print(f"Merged List: {final_nums_array} | Total Cost: {final_total_cost}")
